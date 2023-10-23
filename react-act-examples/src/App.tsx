c26265121f75642638f4a6fa3c6d2604cec93d8a
lauralen <laurale@wix.com>
2023-10-23 14:41:06 +0300

    WIP timers

--- react-act-examples/src/App.tsx
+++ react-act-examples/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Timers from './timers/Timers';
 
 function App() {
   return (
@@ -19,6 +20,8 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <Timers/>
     </div>
   );
 }
---
+++ react-act-examples/src/timers/Timers.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Timers = () => {
+  const [content, setContent] = React.useState(1);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => setContent(content + 1), 3000);
+
+    return () => clearInterval(interval);
+  }, [content]);
+
+  return (
+    <div>
+      <p>Timers</p>
+      <p>{content}</p>
+    </div>
+  );
+};
+
+export default Timers;
