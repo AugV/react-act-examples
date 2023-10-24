78726e3302b8972e782983528cfb8b632c2389a4
lauralen <laurale@wix.com>
2023-10-24 10:26:31 +0300

    add test name

--- src/noAwait/NoAwait.test.jsx
+++ src/noAwait/NoAwait.test.jsx
@@ -4,34 +4,38 @@ import NoAwait from "./NoAwait";
 
 // ❗ False positives also occur when not awaiting eventually
 
-it("❌ false positive when not awaiting waitFor", async () => {
-  render(<NoAwait />);
+describe("❌ false positive when not awaiting waitFor", async () => {
+  it("toggles content on button click", async () => {
+    render(<NoAwait />);
 
-  await userEvent.click(screen.getByRole("button"));
+    await userEvent.click(screen.getByRole("button"));
 
-  await waitFor(() => {
-    expect(screen.getByText("Content")).toBeTruthy();
-  });
+    await waitFor(() => {
+      expect(screen.getByText("Content")).toBeTruthy();
+    });
 
-  await userEvent.click(screen.getByRole("button"));
+    await userEvent.click(screen.getByRole("button"));
 
-  waitFor(() => {
-    expect(screen.getByText("Content")).toBeFalsy();
+    waitFor(() => {
+      expect(screen.getByText("Content")).toBeFalsy();
+    });
   });
 });
 
-it("✅ test detects the bug and fails when awaiting waitFor", async () => {
-  render(<NoAwait />);
+describe("✅ test detects the bug and fails when awaiting waitFor", async () => {
+  it("toggles content on button click", async () => {
+    render(<NoAwait />);
 
-  await userEvent.click(screen.getByRole("button"));
+    await userEvent.click(screen.getByRole("button"));
 
-  await waitFor(() => {
-    expect(screen.getByText("Content")).toBeTruthy();
-  });
+    await waitFor(() => {
+      expect(screen.getByText("Content")).toBeTruthy();
+    });
 
-  await userEvent.click(screen.getByRole("button"));
+    await userEvent.click(screen.getByRole("button"));
 
-  await waitFor(() => {
-    expect(screen.getByText("Content")).toBeFalsy();
+    await waitFor(() => {
+      expect(screen.getByText("Content")).toBeFalsy();
+    });
   });
 });
