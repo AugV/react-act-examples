a8558ea597bb8ef9f2d186ab36e852f027f77bb8
Augustinas Vainius <augustinasv@wix.com>
2023-10-27 11:02:14 +0300

    overlapping update

--- src/presentation/5_Overlapping.test.jsx
+++ src/presentation/5_Overlapping.test.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { render, screen, act, waitFor } from "@testing-library/react";
+import { Simulate } from "react-dom/test-utils";
 
 const Timers = () => {
   const [content, setContent] = React.useState(1);
@@ -14,6 +15,7 @@ const Timers = () => {
     <div>
       <p>Timers</p>
       <p>{content}</p>
+      <button>Button</button>
     </div>
   );
 };
@@ -26,6 +28,23 @@ it("increments number at intervals", async () => {
   waitFor(async () => {
     expect(screen.getByText("1")).toBeTruthy();
   });
+
+
+
+  expect(screen.getByText("2")).toBeTruthy();
+});
+
+
+it("increments number at intervals", async () => {
+  render(<Timers />);
+  const clickButton = async (element) => {
+    return act(async () => {
+      Simulate.click(element);
+    })
+  }
+  
+  clickButton(screen.getByRole('button'))
+
   await act(async () => {
     jest.runOnlyPendingTimers();
   });
